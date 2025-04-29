dabfcdbbb718dddf750091a664605133ca7e31a8
MOHD Araf <157881351+Mohd-Araf@users.noreply.github.com>
2025-04-30 01:48:16 +0600

    second commit

--- library/library/settings.py
+++ library/library/settings.py
@@ -121,3 +121,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#newadd by araf
+import os
+
+# Static files (CSS, JavaScript, Images)
+
+
+# Where Django will collect all static files (when you run collectstatic)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Where you will keep your own static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
